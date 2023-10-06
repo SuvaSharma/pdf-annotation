78e4305fd3eabe342d8e1e750a52ef49d3865f74
SuvaSharma <sharmasuva1@gmail.com>
2023-10-06 16:31:07 +0545

    new changes

--- PDF_VIEWER/settings.py
+++ PDF_VIEWER/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Media files (user-uploaded files like PDFs)
 
