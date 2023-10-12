70eb6ac86370d8ce66b78034b58b9a00015af87a
SuvaSharma <sharmasuva1@gmail.com>
2023-10-12 12:48:08 +0545

    allare

--- PDF_VIEWER/settings.py
+++ PDF_VIEWER/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' , 'static')
 STATICFILES_DIRS = os.path.join(BASE_DIR ,'static'),
 
